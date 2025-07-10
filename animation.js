8e1d29072a73156557f8cbb14d4b7c9d0a771e58
SilvioYamada <silvioyamada@icloud.com>
2025-07-10 17:00:51 -0300

    Adicionando atualizações em index.html e style.css. E novo arquivo animation.js menu mobile do icone fechar.

---
+++ animation.js
@@ -0,0 +1,7 @@
+const menuDiv = document.getElementById('menu-mobile')
+const btnMenu = document.getElementById('btn-menu-mobile')
+menuDiv.addEventListener('click', animationMenu)
+function animationMenu() {
+    menuDiv.classList.toggle('open')
+    btnMenu.classList.toggle('active')
+}
