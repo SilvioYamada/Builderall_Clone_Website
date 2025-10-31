4b21af8124f9c37e69c9e7a6723d687627dd3a58
SilvioYamada <silvioyamada@icloud.com>
2025-10-31 18:29:22 -0300

    commit

--- animation.js
+++ animation.js
@@ -1,13 +1,13 @@
-const menuDiv = document.getElementById('menu-mobile')
-const btnMenu = document.getElementById('btn-menu-mobile')
-menuDiv.addEventListener('click', animationMenu)
+const menuDiv = document.getElementById("menu-mobile");
+const btnMenu = document.getElementById("btn-menu-mobile");
+menuDiv.addEventListener("click", animationMenu);
 function animationMenu() {
-    menuDiv.classList.toggle('open')
-    btnMenu.classList.toggle('active')
+  menuDiv.classList.toggle("open");
+  btnMenu.classList.toggle("active");
 }
 
-const video = document.getElementById('#video');
+const video = document.getElementById("#video");
 
-video.addEventListener('mouseout', () => {
-    video.removeAttribute('controls', 'controls');
+video.addEventListener("mouseout", () => {
+  video.removeAttribute("controls", "controls");
 });
